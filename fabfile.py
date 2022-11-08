ec340853051df72e48c661673cb81785080b9753
Tammy Bailey <tammy.bailey@mongodb.com>
2022-11-08 20:40:57 +1100

    Implemented the populate fabfile function and two helper functions for accessing settings in the .testy configuration file

---
+++ fabfile.py
@@ -0,0 +1,63 @@
+# fabfile.py
+# Remote management commands for testy: A WiredTiger 24/7 workload testing framework.
+
+import configparser
+from fabric import task
+from invoke.exceptions import Exit
+
+testy_config = ".testy"
+
+
+# ---------------------------------------------------------------------------------------
+# fabfile tasks
+# ---------------------------------------------------------------------------------------
+
+# Run the populate function as defined in the workload interface file "workload.sh".
+@task
+def populate(c, workload=None):
+
+    if not workload:
+        raise Exit("Please specify a workload argument.")
+
+    wif = get_config_value("environment", "workload_dir") + "/" + workload + ".sh"
+    command = get_config_env("environment") + " bash " + wif + " populate"
+    status = c.sudo(command,  user="testy", warn=True)
+
+    if status.return_code == 0:
+        print("populate succeeded for workload '" + workload + "'")
+    else:
+        print("populate failed for workload '" + workload + "'")
+
+
+# ---------------------------------------------------------------------------------------
+# helper functions
+# ---------------------------------------------------------------------------------------
+
+# Return the value corresponding to the specified key from the specified section
+# of the testy configuration file.
+def get_config_value(section, key):
+
+    config = configparser.ConfigParser()
+    config.read(testy_config)
+
+    if section not in config.sections():
+        raise Exit("No section '" + section + "' in file '" + testy_config + "'.")
+    if not config.has_option(section, key):
+        raise Exit("No option '" + option + "' in section '" + section + "'.")
+
+    return config.get(section, key)
+
+# Return the key/value pairs in the environment section of the testy configuration file
+# as a single string of shell environment values.    
+def get_config_env(section):
+
+    config = configparser.ConfigParser()
+    config.read(testy_config)
+
+    if section not in config.sections():
+        raise Exit("No section '" + section + "' in file '" + testy_config + "'.")
+
+    env = ""
+    for k, v in config.items(section):
+        env += k + "=" + v + " "
+    return env
