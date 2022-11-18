0ac4658dccf4eaa3113751b29184f059de9d3af4
Ruby Chen <ruby.chen@mongodb.com>
2022-11-18 16:48:18 +1100

    Implementing unpacking of archives in a helper python function.

---
+++ scripts/testy_unpack.py
@@ -0,0 +1,16 @@
+import shutil
+import sys
+
+# Unpacks an archive to a given destination. The shutil library handles the archive format.
+def unpack_archive(src, dest):
+    shutil.unpack_archive(src, dest)
+
+# This allows us to call script functions by name from the command line with an
+# arbitrary number of parameters. Example usage is:
+# 
+#   $ python3 testy_parse.py get_env '/srv/testy/.testy' 'environment'
+#   $ python3 testy_parse.py get_value '/srv/testy/.testy' 'application' 'user'
+#
+if __name__ == "__main__":
+
+    globals()[sys.argv[1]](*sys.argv[2:])
