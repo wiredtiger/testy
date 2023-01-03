f6b5ff8c31ccf48aa25b2b4e224103ddb14aafab
Ruby Chen <ruby.chen@mongodb.com>
2023-01-04 10:31:44 +1100

    Addressing pr comments.

--- fabfile.py
+++ fabfile.py
@@ -571,10 +571,10 @@ def update_wiredtiger(c, branch):
     old_branch = None
     with c.cd(wt_home_dir):
         result = c.run("git rev-parse --abbrev-ref HEAD", hide=True)
-        commit = c.run("git rev-parse HEAD", hide=True)
         if not result.stdout:
             raise Exit(f"Error: {wiredtiger} is not currently on a branch.")
         old_branch = result.stdout.strip()
+        commit = c.run("git rev-parse HEAD", hide=True)
         commit_hash = commit.stdout.strip()
 
     # Check out branch from GitHub.
@@ -589,10 +589,9 @@ def update_wiredtiger(c, branch):
         print(f"\nAttempting to restore branch '{old_branch}' ...")
         # If we are on the same branch and the new commits breaks, use an older working commit.
         if old_branch == branch:
-            print(commit_hash, "commit")
             if git_checkout(c, wt_home_dir, commit_hash) and \
             build_wiredtiger(c, wt_home_dir, wt_build_dir, commit_hash):
-                print(f"Restored {wiredtiger} to previous {branch} commit {commit_hash} ")
+                print(f"Restored {wiredtiger} branch '{branch}' to the previous commit.")
         elif git_checkout(c, wt_home_dir, old_branch) and \
            build_wiredtiger(c, wt_home_dir, wt_build_dir, old_branch):
             print(f"Restored {wiredtiger} to branch '{branch}'.")
