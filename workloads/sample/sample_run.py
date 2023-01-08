af02017eec0da6ae9aeeb6bf17dadcad0c425a9e
Ruby Chen <ruby.chen@mongodb.com>
2023-01-09 09:23:49 +1100

    Implement single delete of workgen sample workload.

--- workloads/sample/sample_run.py
+++ workloads/sample/sample_run.py
@@ -85,7 +85,7 @@ opremove= Operation(Operation.OP_REMOVE, table)
 ops = opremove * 10 + Operation(Operation.OP_SLEEP, "10")
 tremover= Thread(ops)
 workload= Workload(context, tremover * 10)
-workload.options.run_time = 100000
+workload.options.run_time = 2147483647
 workload.run(connection)
 
 # Finish with a checkpoint to make all data durable.
