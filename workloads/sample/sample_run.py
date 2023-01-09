aad8d282661446a180ab3a1309650eb551a92f87
Ruby Chen <ruby.chen@mongodb.com>
2023-01-09 15:28:15 +1100

    Using key/values for operations.

--- workloads/sample/sample_run.py
+++ workloads/sample/sample_run.py
@@ -53,7 +53,8 @@ update_op_3 = Operation(Operation.OP_UPDATE, Key(Key.KEYGEN_PARETO, 512, ParetoO
 update_thread = Thread(10*update_op_1 + 5*update_op_2 + update_op_3)
 
 # Insert single read
-read_op = Operation(Operation.OP_SEARCH, table) * 10 + Operation(Operation.OP_SLEEP, "10")
+read_op = Operation(Operation.OP_SEARCH, Key(Key.KEYGEN_APPEND, 512), Value(1)) * 10 + \
+          Operation(Operation.OP_SLEEP, "10")
 read_thread = Thread(read_op)
 
 # Define the workload operations.
