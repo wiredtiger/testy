92e9fbec094569ac98d142333cc493ab2e4bc5ba
Ubuntu <ubuntu@ip-10-0-4-189.ec2.internal>
2023-01-10 02:08:10 +0000

    Reducing the amount of delete operations.

--- workloads/sample/sample_run.py
+++ workloads/sample/sample_run.py
@@ -58,12 +58,13 @@ read_op = Operation(Operation.OP_SEARCH, Key(Key.KEYGEN_APPEND, 512), Value(1)) 
 read_thread = Thread(read_op)
 
 # Insert single delete
-delete_op = Operation(Operation.OP_REMOVE, table) * 10 + Operation(Operation.OP_SLEEP, "10")
+delete_op = Operation(Operation.OP_REMOVE, Key(Key.KEYGEN_APPEND, 512), Value(1)) + \
+Operation(Operation.OP_SLEEP, "10")
 delete_thread = Thread(delete_op)
 
 # Define the workload operations.
 workload = Workload(context, 10*insert_thread + 10*update_thread + 10*read_thread + \
-     10*delete_thread)
+     5*delete_thread)
 
 # Add a prefix to the table names.
 workload.options.create_prefix = "table_"
