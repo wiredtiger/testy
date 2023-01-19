efedbc69aa525df7a9efd4fa3d1ddb297870d3bf
Etienne Petrel <etienne.petrel@mongodb.com>
2023-01-20 09:16:10 +1100

    cleanup

--- workloads/sample/sample_backup.sh
+++ workloads/sample/sample_backup.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+
 # SSH Keys used by the testy user.
 ssh_key_name=etienne-rsa
-# Moutning point.
-mounting_point=/dev/xvdf1
+# Virtual block device.
+virtual_device=/dev/xvdf1
 # Where the backup is mounted.
-folder=/mnt/backup
+backup_folder=/mnt/backup
 
 # Retrieve the testy instance id using the ssh key.
 testy_instance_id=$(aws ec2 describe-instances --filters "Name=key-name,Values=$ssh_key_name" --query "Reservations[*].Instances[*].InstanceId" --output text)
@@ -32,10 +33,8 @@ echo "Testy snapshot ID: $snapshot_id"
 
 cloudwatch=$(aws cloudwatch put-metric-data --metric-name backup --dimensions Instance="$testy_instance_id" --namespace "backup" --value 1)
 
-# Check if anything is already mounted. This means a backup is in progress.
-# ssh ubuntu@ec2-3-106-126-176.ap-southeast-2.compute.amazonaws.com 'ls /dev/xvdf1'
-if ls $mounting_point > /dev/null; then
-# if [ $? == 0 ]; then
+# Check if the virtual device is present. If it is, it can mean a backup is in progress or a volume has not been detached.
+if ls $virtual_device > /dev/null; then
     echo "A backup is already in progress!"
     exit 1
 fi
@@ -66,7 +65,7 @@ fi
 volume_id=$(less "$volume" | jq -r .VolumeId)
 echo "Snapshot volume ID: $volume_id"
 
-# We need to wait for the volume to be ready for use.
+# We need to wait for the volume to be ok.
 echo -e "\nWaiting for the volume $volume_id to be ok..."
 volume_status=$(aws ec2 describe-volume-status --volume-ids "$volume_id" --query "VolumeStatuses[*].VolumeStatus.Status" --output text)
 until [ "$volume_status" == "ok" ]
@@ -76,6 +75,7 @@ do
 volume_status=$(aws ec2 describe-volume-status --volume-ids "$volume_id" --query "VolumeStatuses[*].VolumeStatus.Status" --output text)
 done
 echo "volume_status: $volume_status"
+
 # Check the volume is available.
 echo -e "\nWaiting for the volume $volume_id to be available..."
 volume_state=$(aws ec2 describe-volumes --volume-ids "$volume_id" --query "Volumes[*].State" --output text)
@@ -88,9 +88,6 @@ done
 echo "volume_state: $volume_state"
 
 # Attach the volume to the instance.
-# What if something is already attached here? We could check if there is another volume attached.
-# Either wait, or attach to a different mounting point.
-# Send an alert if that's the case. And exit here. 
 echo -e "\nAttaching the volume $volume_id to the instance $testy_instance_id..."
 attach=$(aws ec2 attach-volume --device xvdf --instance-id "$testy_instance_id" --volume-id "$volume_id")
 if [ $? != 0 ]; then
@@ -101,22 +98,18 @@ fi
 # Connect to the instance and mount the volume. Wait for some time after attaching the volume.
 sleep 10
 
-echo -e "\nCreating $folder..."
-# ssh ubuntu@ec2-3-106-126-176.ap-southeast-2.compute.amazonaws.com 'sudo mkdir -p /mnt/backup'
-sudo mkdir -p "$folder"
-echo "Mounting $mounting_point to $folder..."
-# ssh ubuntu@ec2-3-106-126-176.ap-southeast-2.compute.amazonaws.com 'sudo mount /dev/xvdf1 /mnt/backup'
-sudo mount $mounting_point $folder;
+echo -e "\nCreating $backup_folder..."
+sudo mkdir -p "$backup_folder"
+echo "Mounting $virtual_device to $backup_folder..."
+sudo mount $virtual_device $backup_folder;
 
-# Verify a table.
+# Call validate.
 echo -e "\nValidating database..."
 validation=0
-# ssh ubuntu@ec2-3-106-126-176.ap-southeast-2.compute.amazonaws.com '/mnt/backup/srv/testy/workloads/sample/sample.sh validate'
-# if [ $? == 0 ]; then
-if ! $folder/srv/testy/workloads/sample/sample.sh validate; then
+if ! $backup_folder/srv/testy/workloads/sample/sample.sh validate; then
     validation=-1
     cloudwatch=$(aws cloudwatch put-metric-data --metric-name validation --dimensions Instance="$testy_instance_id" --namespace "validation" --value "$validation")
-    echo "!!VALIDATION FAILED!!"
+    echo "Validation failed!"
     # Mark the snapshot as corrupted.
     aws ec2 create-tags --resources "$snapshot_id" --tags Key=analysis,Value=corrupted
 else
@@ -135,9 +128,8 @@ else
 fi
 
 # When the validation is done, unmount and detach the volume.
-echo -e "\nUnmounting $mounting_point..."
-# ssh ubuntu@ec2-3-106-126-176.ap-southeast-2.compute.amazonaws.com 'sudo umount /dev/xvdf1'
-sudo umount "$mounting_point"
+echo -e "\nUnmounting $virtual_device..."
+sudo umount "$virtual_device"
 echo "Detaching $volume_id..."
 detach_volume=$(aws ec2 detach-volume --volume-id "$volume_id" --query "State" --output text)
 if [ $? != 0 ]; then
