5ae54244c3de8f001920d65e3aa979b52ec367af
Etienne Petrel <etienne.petrel@mongodb.com>
2023-01-20 09:04:37 +1100

    Rename script for backup logic

--- workloads/sample/sample.sh
+++ workloads/sample/sample.sh
@@ -1,16 +1,6 @@
 #! /bin/bash
 # Initial sample workload for use with the testy framework.
 
-backup() {
-    # Trigger the backup.
-    ${workload_dir}/sample/my_script.sh
-    # sudo ${wt_build_dir}/wt -h ${database_dir} backup "$1"
-
-    if test "$?" -ne "0"; then
-        echo "FAILED BAKCUP SCRIPT"
-    fi
-}
-
 describe() {
     echo "A sample workload for use with the testy framework."
 }
@@ -23,6 +13,10 @@ populate() {
 run() {
     export PYTHONPATH=${wt_build_dir}/bench/workgen:${wt_build_dir}/lang/python:$PYTHONPATH
     python3 ${workload_dir}/sample/sample_run.py --home ${database_dir} --keep
+}
+
+backup() {
+    ${workload_dir}/sample/sample_backup.sh
 }
 
 validate() {
