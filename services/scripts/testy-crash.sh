5e779fff8d7ba7cbdd974b093af41272ca1b6d1c
Etienne Petrel <etienne.petrel@mongodb.com>
2023-01-25 23:10:20 +1100

    Implement the crash logic

--- services/scripts/testy-crash.sh
+++ services/scripts/testy-crash.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 
 main() {
-    :
+    # Exit if the backup service is active.
+    local _workload=$1
+    if systemctl is-active --quiet testy-backup@"$_workload".service; then
+        echo "The backup service is currently running."
+        exit 1
+    fi
+
+    # Retrieve the Python process and send a SIGKILL.
+    if ! pgrep -U testy python &> /dev/null; then
+        echo "Failed to find running Python process."
+        exit 1
+    fi
+    local _python_pid
+    _python_pid=$(pgrep -U testy python)
+
+    if ! sudo kill -SIGKILL "$_python_pid"; then
+        echo "Failed to kill Python process $_python_pid"
+        exit 1
+    fi
 }
 
 # Run main function.
