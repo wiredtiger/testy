b409a8f949645e4c04f1a1a79c388a02d1446fd6
Etienne Petrel <etienne.petrel@mongodb.com>
2023-02-03 15:47:17 +1100

    Missing space

--- services/scripts/testy-metrics.sh
+++ services/scripts/testy-metrics.sh
@@ -9,6 +9,6 @@ _aws_endpoint="http://169.254.169.254/latest/meta-data/"
 _instance_id=$(curl ${_aws_endpoint}/instance-id 2> /dev/null)
 
 if ! aws cloudwatch put-metric-data --metric-name "$_metric_name" --dimensions Instance="$_instance_id" --namespace "$_metric_namespace" --value "$_metric_value"; then
-    echo "Error: Failed calling put-metric-data (instance: $_instance_id, metric name: $_metric_name, metric value:$_metric_value, metric namespace: $_metric_namespace)."
+    echo "Error: Failed calling put-metric-data (instance: $_instance_id, metric name: $_metric_name, metric value: $_metric_value, metric namespace: $_metric_namespace)."
     exit 1
 fi
