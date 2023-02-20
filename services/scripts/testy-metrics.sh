460d101a5fc7a51183a4cc8157553e271fc6597c
Ruby Chen <ruby.chen@mongodb.com>
2023-02-20 17:11:13 +1100

    Push logs to dashboard

--- services/scripts/testy-metrics.sh
+++ services/scripts/testy-metrics.sh
@@ -12,3 +12,9 @@ if ! aws cloudwatch put-metric-data --metric-name "$_metric_name" --dimensions I
     echo "Error: Failed calling put-metric-data (instance: $_instance_id, metric name: $_metric_name, metric value: $_metric_value, metric namespace: $_metric_namespace)."
     exit 1
 fi
+
+# This requires a file called logs in the current directory - can modify this dependent on where the logs are 
+if ! aws logs put-log-events --log-group-name testy-logs --log-stream-name 0000001 --log-events file://logs; then
+    echo "Error - cannot put logs"
+    exit 1
+fi
