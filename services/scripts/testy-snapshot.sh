71d801231a1cf94bee3550aeedbee5a5f88e851c
Etienne Petrel <etienne.petrel@mongodb.com>
2023-10-23 12:21:56 +1100

    Delete the snapshot once validated

--- services/scripts/testy-snapshot.sh
+++ services/scripts/testy-snapshot.sh
@@ -87,17 +87,21 @@ main() {
                         --tags "Key=Validation,Value=none Key=InstanceID,Value=$_instance_id"
 
     echo "Running validation script '$_validation_script' on volume '$_volume_id'."
-    var=$(date +%s%3N)
+    ts=$(date +%s%3N)
     if validate_database "$_validation_script" "$_mount_point" "$_snapshot_id" "$_volume_id"; then
         echo "Successfully validated database backup snapshot '$_snapshot_id'."
         aws logs put-log-events --log-group-name testy-logs \
                                 --log-stream-name testy-logs --log-events \
-            timestamp=$var,message="Backup snapshot validation succeeded. $_snapshot_id. $_instance_id"
+            timestamp=$ts,message="Backup snapshot validation succeeded. $_snapshot_id. $_instance_id"
+        # We can delete the snapshot now it has been validated.
+        if ! aws ec2 delete-snapshot --snapshot-id "$_snapshot_id"; then
+            echo "Error: Failed to delete snapshot '$snapshot_id'."
+        fi
     else
         echo "Validation failed for database backup snapshot '$_snapshot_id'."
         aws logs put-log-events --log-group-name testy-logs \
                                 --log-stream-name testy-logs --log-events \
-            timestamp=$var,message="Backup snapshot validation failed. $_snapshot_id. $_instance_id"
+            timestamp=$ts,message="Backup snapshot validation failed. $_snapshot_id. $_instance_id"
     fi
 
     # Unmount the device, detach the volume and delete it when the validation is done. We
