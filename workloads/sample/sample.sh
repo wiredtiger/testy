f22ddc7abd666448cbbee4881dc6b5b0f40620e5
Etienne Petrel <etienne.petrel@mongodb.com>
2024-03-11 01:11:30 +0000

    Add information about disk usage when validating

--- workloads/sample/sample.sh
+++ workloads/sample/sample.sh
@@ -21,9 +21,11 @@ validate() {
     set -o pipefail
     export PYTHONPATH=${wt_build_dir}/lang/python:${wt_home_dir}/tools:$PYTHONPATH
     echo "Running verify..."
-    ${wt_build_dir}/wt -h "$1/$database_dir" -R verify 2>&1 | sudo tee ${failure_dir}/${failure_file}
+    df -h > ${failure_dir}/${failure_file}
+    du -h "$1/$database_dir" >> ${failure_dir}/${failure_file}
+    ${wt_build_dir}/wt -h "$1/$database_dir" -R verify 2>&1 | sudo tee -a ${failure_dir}/${failure_file}
     echo "Validating mirrors..."
-    python3 ${wt_home_dir}/bench/workgen/validate_mirror_tables.py "$1/$database_dir" 2>&1 | sudo tee ${failure_dir}/${failure_file}
+    python3 ${wt_home_dir}/bench/workgen/validate_mirror_tables.py "$1/$database_dir" 2>&1 | sudo tee -a ${failure_dir}/${failure_file}
     sudo rm -f ${failure_dir}/${failure_file}
 }
 
